da4c854e3fd1d45fc167d04f9d1f6f9275e1efcf
Ilya Gerasimchik <ilyagerasimchik@gmail.com>
2021-09-14 11:41:57 +0300

    Add storage command line parameter.

--- FileCabinetApp/Services/FileCabinetFilesystemService.cs
+++ FileCabinetApp/Services/FileCabinetFilesystemService.cs
@@ -9,49 +9,119 @@ using FileCabinetApp.SnapshotServices;
 
 namespace FileCabinetApp.Services
 {
+    /// <summary>
+    /// Class <c>FileCabinetFilesystemService</c>.
+    /// </summary>
+    /// <seealso cref="FileCabinetApp.Services.IFileCabinetService" />
     public class FileCabinetFilesystemService : IFileCabinetService
     {
+        /// <summary>
+        /// The file stream.
+        /// </summary>
         private readonly FileStream fileStream;
 
+        /// <summary>
+        /// Initializes a new instance of the <see cref="FileCabinetFilesystemService"/> class.
+        /// </summary>
+        /// <param name="fileStream">The file stream.</param>
         public FileCabinetFilesystemService(FileStream fileStream)
         {
             this.fileStream = fileStream;
         }
+
+        /// <summary>
+        /// Creates the record.
+        /// </summary>
+        /// <param name="container">The container of parameters.</param>
+        /// <returns>
+        /// Return Id of created record.
+        /// </returns>
         public int CreateRecord(ParametersContainer container)
         {
             throw new NotImplementedException();
         }
 
+        /// <summary>
+        /// Edits the record.
+        /// </summary>
+        /// <param name="id">The identifier.</param>
+        /// <param name="container">The container of parameters.</param>
+        /// <exception cref="System.NotImplementedException">Not implemented.</exception>
         public void EditRecord(int id, ParametersContainer container)
         {
             throw new NotImplementedException();
         }
 
+        /// <summary>
+        /// Gets all records in File Cabinet.
+        /// </summary>
+        /// <returns>
+        /// Return array of <c>FileCabinetRecord</c>.
+        /// </returns>
+        /// <exception cref="System.NotImplementedException">Not implemented.</exception>
         public IReadOnlyCollection<FileCabinetRecord> GetRecords()
         {
             throw new NotImplementedException();
         }
 
+        /// <summary>
+        /// Gets the stat of users.
+        /// </summary>
+        /// <returns>
+        /// Return count of records in File Cabinet.
+        /// </returns>
+        /// <exception cref="System.NotImplementedException">Not implemented.</exception>
         public int GetStat()
         {
             throw new NotImplementedException();
         }
 
+        /// <summary>
+        /// Finds the records by the first name.
+        /// </summary>
+        /// <param name="firstName">The first name.</param>
+        /// <returns>
+        /// Return array of records.
+        /// </returns>
+        /// <exception cref="System.NotImplementedException">Not implemented.</exception>
         public IReadOnlyCollection<FileCabinetRecord> FindByFirstName(string firstName)
         {
             throw new NotImplementedException();
         }
 
+        /// <summary>
+        /// Finds the records by the last name.
+        /// </summary>
+        /// <param name="lastName">The last name.</param>
+        /// <returns>
+        /// Return array of records.
+        /// </returns>
+        /// <exception cref="System.NotImplementedException">Not implemented.</exception>
         public IReadOnlyCollection<FileCabinetRecord> FindByLastName(string lastName)
         {
             throw new NotImplementedException();
         }
 
+        /// <summary>
+        /// Finds the records by date of birthday.
+        /// </summary>
+        /// <param name="dateOfBirth">The date of birthday.</param>
+        /// <returns>
+        /// Return array of records.
+        /// </returns>
+        /// <exception cref="System.NotImplementedException">Not implemented.</exception>
         public IReadOnlyCollection<FileCabinetRecord> FindByDateOfBirthName(DateTime dateOfBirth)
         {
             throw new NotImplementedException();
         }
 
+        /// <summary>
+        /// Makes the snapshot.
+        /// </summary>
+        /// <returns>
+        /// Return <c>FileCabinetServiceSnapshot</c>.
+        /// </returns>
+        /// <exception cref="System.NotImplementedException">Not implemented.</exception>
         public IFileCabinetServiceSnapshot MakeSnapshot()
         {
             throw new NotImplementedException();
